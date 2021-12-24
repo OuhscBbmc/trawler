586f00dc1577e9c3cdfd1b45b85b106fa7e8758b
Will Beasley <wibeasley@hotmail.com>
2021-12-23 19:32:26 -0600

    fix indention level
    ref #3

--- R/load-checks.R
+++ R/load-checks.R
@@ -9,71 +9,71 @@
 #'
 #' @export
 load_checks <- function (path_checks) {
-    checkmate::assert_file_exists(path_checks, extension = c("yml", "yaml"))
+  checkmate::assert_file_exists(path_checks, extension = c("yml", "yaml"))
 
-    checks <- config::get(file = path_checks)
+  checks <- config::get(file = path_checks)
 
-    smells <- load_smells(checks)
-    rules  <- load_rules(checks)
+  smells <- load_smells(checks)
+  rules  <- load_rules(checks)
 
-    list(
-      ds_smell            = smells$ds_smell,
-      ds_smell_inactive   = smells$ds_smell_inactive,
-      ds_rule             = rules$ds_rule,
-      ds_rule_inactive    = rules$ds_rule_inactive
-    )
+  list(
+    ds_smell            = smells$ds_smell,
+    ds_smell_inactive   = smells$ds_smell_inactive,
+    ds_rule             = rules$ds_rule,
+    ds_rule_inactive    = rules$ds_rule_inactive
+  )
 }
 
 #' @importFrom rlang .data
 load_smells <- function (checks) {
-    ds_smell <-
-      checks$smells |>
-      purrr::map_df(tibble::as_tibble) |>
-      dplyr::filter(.data$active) |>
-      dplyr::mutate(
-        debug         = dplyr::coalesce(.data$debug, FALSE),
-      )
-
-    ds_smell_inactive <-
-      checks$smells |>
-      purrr::map_df(tibble::as_tibble) |>
-      dplyr::filter(!.data$active)
-
-    # The smell columns (in the yaml checks file) should be correct.
-    checkmate::assert_subset(
-      colnames(ds_smell),
-      c("check_name", "description", "priority", "active", "debug", "bound_lower", "bound_upper", "bounds_template", "value_template", "equation")
+  ds_smell <-
+    checks$smells |>
+    purrr::map_df(tibble::as_tibble) |>
+    dplyr::filter(.data$active) |>
+    dplyr::mutate(
+      debug         = dplyr::coalesce(.data$debug, FALSE),
     )
 
-    testit::assert(
-      "The count of distinct rule columns (in the yaml checks file) should be 10.",
-      ncol(ds_smell) == 10L
+  ds_smell_inactive <-
+    checks$smells |>
+    purrr::map_df(tibble::as_tibble) |>
+    dplyr::filter(!.data$active)
+
+  # The smell columns (in the yaml checks file) should be correct.
+  checkmate::assert_subset(
+    colnames(ds_smell),
+    c("check_name", "description", "priority", "active", "debug", "bound_lower", "bound_upper", "bounds_template", "value_template", "equation")
+  )
+
+  testit::assert(
+    "The count of distinct rule columns (in the yaml checks file) should be 10.",
+    ncol(ds_smell) == 10L
+  )
+
+  ds_smell <-
+    ds_smell |>
+    dplyr::mutate(
+      boundaries            = sprintf(.data$bounds_template, .data$bound_lower, .data$bound_upper),
     )
 
-    ds_smell <-
-      ds_smell |>
-      dplyr::mutate(
-        boundaries            = sprintf(.data$bounds_template, .data$bound_lower, .data$bound_upper),
-      )
+  # table(ds_smell$check_name)
+  # OuhscMunge::verify_value_headstart(ds_smell)
+  checkmate::assert_character(ds_smell$check_name      , any.missing = FALSE , pattern="^.{4,99}$"  , unique = TRUE)
+  checkmate::assert_character(ds_smell$description     , any.missing = FALSE , pattern="^.{4,255}$" , unique = TRUE)
+  checkmate::assert_integer(  ds_smell$priority        , any.missing = FALSE , lower=1, upper=5     )
+  checkmate::assert_logical(  ds_smell$active          , any.missing = FALSE                        )
+  checkmate::assert_logical(  ds_smell$debug           , any.missing = FALSE                        )
+  checkmate::assert_numeric(  ds_smell$bound_lower     , any.missing = FALSE                        )
+  checkmate::assert_numeric(  ds_smell$bound_upper     , any.missing = FALSE                        )
+  checkmate::assert_character(ds_smell$bounds_template , any.missing = FALSE , pattern="^.{2,255}$" )
+  checkmate::assert_character(ds_smell$value_template  , any.missing = FALSE , pattern="^.{2,255}$" )
+  checkmate::assert_character(ds_smell$equation        , any.missing = FALSE , pattern="^.{5,}$"    , unique = TRUE)
+  checkmate::assert_character(ds_smell$boundaries      , any.missing = FALSE , pattern="^.{6,}$"  )
 
-    # table(ds_smell$check_name)
-    # OuhscMunge::verify_value_headstart(ds_smell)
-    checkmate::assert_character(ds_smell$check_name      , any.missing = FALSE , pattern="^.{4,99}$"  , unique = TRUE)
-    checkmate::assert_character(ds_smell$description     , any.missing = FALSE , pattern="^.{4,255}$" , unique = TRUE)
-    checkmate::assert_integer(  ds_smell$priority        , any.missing = FALSE , lower=1, upper=5     )
-    checkmate::assert_logical(  ds_smell$active          , any.missing = FALSE                        )
-    checkmate::assert_logical(  ds_smell$debug           , any.missing = FALSE                        )
-    checkmate::assert_numeric(  ds_smell$bound_lower     , any.missing = FALSE                        )
-    checkmate::assert_numeric(  ds_smell$bound_upper     , any.missing = FALSE                        )
-    checkmate::assert_character(ds_smell$bounds_template , any.missing = FALSE , pattern="^.{2,255}$" )
-    checkmate::assert_character(ds_smell$value_template  , any.missing = FALSE , pattern="^.{2,255}$" )
-    checkmate::assert_character(ds_smell$equation        , any.missing = FALSE , pattern="^.{5,}$"    , unique = TRUE)
-    checkmate::assert_character(ds_smell$boundaries      , any.missing = FALSE , pattern="^.{6,}$"  )
-
-    list(
-      ds_smell            = ds_smell,
-      ds_smell_inactive   = ds_smell_inactive
-    )
+  list(
+    ds_smell            = ds_smell,
+    ds_smell_inactive   = ds_smell_inactive
+  )
 }
 
 #' @importFrom rlang .data
